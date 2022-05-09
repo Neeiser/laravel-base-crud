a53a110049445c9a4801a1aad0527c5569c4d347
Neeiser <stortini.study@gmail.com>
2022-05-09 15:42:17 +0200

    Migrate + seed + database structure

--- database/migrations/2022_05_09_114659_create_comics_table.php
+++ database/migrations/2022_05_09_114659_create_comics_table.php
@@ -15,6 +15,13 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 250)->nullable();
+            $table->text('description')->nullable();
+            $table->string('thumb', 250)->nullable();
+            $table->integer('price')->nullable();
+            $table->string('series')->nullable();
+            $table->date('sale_date')->nullable();
+            $table->string('type')->nullable();
             $table->timestamps();
         });
     }
