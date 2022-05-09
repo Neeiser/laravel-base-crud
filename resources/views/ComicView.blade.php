b42f70d02023652448726f73d889f8b0e52ecb0a
Neeiser <stortini.study@gmail.com>
2022-05-09 18:46:13 +0200

    display all comics + some random style

--- resources/views/ComicView.blade.php
+++ resources/views/ComicView.blade.php
@@ -5,8 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    hello
+    <div class="comics-container">
+        @foreach ($comics as $comic)
+            <a href="">
+                <div class="comic-box">
+                    <img src="{{$comic->thumb}}" alt="">
+                    <p>{{$comic->title}}</p>
+                </div>
+            </a>
+        @endforeach
+    </div>
 </body>
 </html>
