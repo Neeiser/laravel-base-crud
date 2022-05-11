8e864375665a2e2187264f6c72b8ac26efc66e3c
Neeiser <stortini.study@gmail.com>
2022-05-11 17:11:01 +0200

    Show view section style (kinda)

--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -1,12 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h3>{{$comic->title}}</h3>
-</body>
-</html>
+@extends('template.base')
+
+@section('pageTitle')
+
+@section('pageMain')
+
+    <div class="comics-container-single">
+        <div class="comic-box-single">
+            <div class="img-comic">
+                <img src="{{$comic->thumb}}" alt="">
+            </div>
+            <p>TITLE: {{$comic->title}}</p>
+            <p>DESC: {{$comic->description}}</p>
+            <p>TYPE: {{$comic->type}}</p>
+            <p>PRICE: {{$comic->price}} $</p>
+            <p>SERIES: {{$comic->series}}</p>
+            <p>DATE: {{$comic->sale_date}}</p>
+        </div>
+    </div>
+
+@endsection
